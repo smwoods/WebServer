fddd76ac3ff075b2b89f26b7539ffc64dc214e5f
smwoods <smwoods@bu.edu>
2015-12-13 19:20:38 -0500

    Cleaned up code

--- server.c
+++ server.c
@@ -18,129 +18,33 @@ The port number is passed as an argument */
 #define IMAGE_JPEG  4
 #define IMAGE_GIF   5
 
-typedef struct cache_entry {
-    int cache_entry;
-    pointer;
-} cache_entry;
+// typedef struct cache_entry {
+//     int cache_entry;
+//     pointer;
+// } cache_entry;
 
 int webcache, multithreading, cache_counter;
 char cachesize[10];
 
-//the thread function
-int connection_handler(int);
 
-int main(int argc, char *argv[]) {
-
-    int webcache, multithreading;
-
-    webcache = 0;
-    multithreading = 0;
-
-    if (argc < 2) {
-        fprintf(stderr,"Error, no port provided.");
-        exit(1);
-    }
-    if (argc > 2){
-        if(strcmp(argv[2], "multi") == 0){
-            multithreading = 1;
-        } else if(strcmp(argv[2], "cache") == 0){
-            webcache = 1;
-        }
-        if(argc > 3){
-            if(strcmp(argv[3], "cache") == 0){
-                webcache = 1;
-            }
-            else if(strcmp(argv[3], "multi") == 0){
-                multithreading = 1;
-            }
-        }
-    }
-    if(webcache == 1){
-        printf("Web cache initiated!!\n");
-        printf("How large should the cache be set to : ");
-        gets(cachesize);
-    }
-    if(multithreading == 1){
-        printf("multithreading initiated!\n");
-    }
-    
-    int port;
-    int sock_fd;
-    int newsock_fd;
-    socklen_t client_len;
-    struct sockaddr_in server_addr;
-    struct sockaddr_in client_addr;
-    
-    // Get port argument, convert to integer
-    port = atoi(argv[1]);
-    
-    // Allocate memory for the server socket address structure
-    memset((char *) &server_addr, 0, sizeof(server_addr));
-    
-    // Set attributes for server socket
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(port);
-    
-    // Try to open an internet socket, save is file descriptor
-    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Error opening socket, exiting...");
-        exit(1);
-    }
-    
-    // Bind server IP and port to the socket
-    if (bind(sock_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-        perror("Error binding socket, exiting...");
-        exit(1);
-    }
-    
-    // Allow the socket to accept connections
-    listen(sock_fd, 5);
-    
-    
-    puts("Waiting for incoming connections...");
-    client_len = sizeof(client_addr);
-    // pthread_t thread_id;
-    
-    // Extract the first connection in queue, create a new socket for
-    // it with the same type and address as the original, return a new
-    // socket file descriptor in newsock_fd
-    while ((newsock_fd = accept(sock_fd, (struct sockaddr *)&client_addr, (socklen_t*)&client_len))) {
-        puts("Connection accepted");
-        
-        connection_handler(newsock_fd);
-        // if( pthread_create( &thread_id , NULL , connection_handler , (void*) &newsock_fd) < 0)
-        // {
-            // perror("could not create thread");
-            // return 1;
-        // }
-        
-        //Now join the thread , so that we dont terminate before the thread
-        //pthread_join( thread_id , NULL);
-        
-    }
-    
-    if (newsock_fd < 0) {
-        perror("Error, connection was not accepted");
-        return -1;
-    }
-    return 0;
-}
-
-int file_exist (char *filename)
-{
-  struct stat   buffer;   
+int file_exist(char *filename) {
+  struct stat buffer;   
   return (stat (filename, &buffer) == 0);
 }
 
 int request_type(char *token) {
     printf("%s:%s\n", "Token", token);
+
     // Find pointer to last dot in token
     const char *dot = strrchr(token, '.');
 
     if (strstr(token, "my-histogram") != NULL){
         return CGI_SCRIPT;
     }
+
+    if (access(token, F_OK) == -1) {
+        return -1;
+    } 
     // If no dot, assume request is for directory
     if (!dot){
         return DIR_LIST;
@@ -176,9 +80,6 @@ void return_404(int newsock_fd){
         perror("Error writing to client");
     }
 }
-
-
-
 
 int cgi_script(int newsock_fd, char *request) {
 
@@ -255,38 +156,27 @@ int cgi_script(int newsock_fd, char *request) {
         }
         else{
             execl(request, (char*) 0);
-            exit(0);
+            exit(-1);
         }
     }
 
     if (pid > 0) {
-      // printf("%s\n", "In parent");
       close(newsock_fd);
-      /* the parent process calls waitpid() on the child */
       if (waitpid(pid, &status, 0) > 0) {
         if (WIFEXITED(status) && !WEXITSTATUS(status)) {
-           // the program terminated normally and executed successfully 
-            // printf("%s\n", "success!");
+           return 0;
         } 
         else if (WIFEXITED(status) && WEXITSTATUS(status)) {
           if (WEXITSTATUS(status) == -1) {
-            printf("%s\n", "Exec failure");
-            /* execl() failed */
-          }
-          else {
-            printf("%s\n", "Child process returning nonzero");
+            printf("%s\n", "Error running exec in child");
+            return -1;
           }
         } 
-        else {
-            printf("%s\n", "Error in child process");
-        }
-      }
-      else {
-        printf("%s\n", "Waitpid failed");
       }
     }
     else {
       printf("%s\n", "Fork failed");
+      return -1
     }
 
     return 0;
@@ -297,7 +187,7 @@ int directory_listing(int newsock_fd, char *request) {
     int status;
 
     char *const arguments[2] = {"directorylisting.cgi", request};
-    printf("%s\n", "IN DIR LISTING");
+
     if ((pid = fork()) == 0) {
         dup2(newsock_fd, STDOUT_FILENO);
         close(newsock_fd);
@@ -305,81 +195,32 @@ int directory_listing(int newsock_fd, char *request) {
         exit(-1);
     }
 
-
     if (pid > 0) {
-      // printf("%s\n", "In parent");
       close(newsock_fd);
-      /* the parent process calls waitpid() on the child */
+
       if (waitpid(pid, &status, 0) > 0) {
         if (WIFEXITED(status) && !WEXITSTATUS(status)) {
-           printf("%s\n", "success!");
+           return 0;
         } 
         else if (WIFEXITED(status) && WEXITSTATUS(status)) {
           if (WEXITSTATUS(status) == -1) {
-            printf("%s\n", "Exec failure");
-            /* execl() failed */
-          }
-          else {
-            printf("%s\n", "Child process returning nonzero");
+            return -1;
           }
         } 
-        else {
-            printf("%s\n", "Error in child process");
-        }
-      }
-      else {
-        printf("%s\n", "Waitpid failed");
       }
     }
 
     else {
       printf("%s\n", "Fork failed");
+      return -1
     }
 
-
-    // DIR *dp;
-    // struct dirent *ep;
-    // char out_buf[2048];
-    // int n;
-
-    // memset(out_buf, 0, 2048);
-    // dp = opendir(request);
-
-    // if (!dp) {
-    //     return_404(newsock_fd);
-    //     return -1;
-    // }
-
-    // strcat(out_buf, "HTTP/1.1 200 OK\nContent-type: text/plain\n\n");
-    // while ((ep = readdir(dp))) {
-    //     strcat(out_buf, ep->d_name);
-    //     strcat(out_buf, "\n");
-    // }
-    // (void) closedir(dp);
-    // n = write(newsock_fd, out_buf, strlen(out_buf));
-
-    // if (n < 0) {
-    //     perror("Error writing to client");
-    // }
 
     return 0;
 }
 
-
-void response(void *message, int msglen, int newsock_fd)
-{
-    char *msg = (char*) message;
-
-    while (msglen > 0)
-    {
-         int len = write(newsock_fd, msg, msglen);
-         if (len <= 0) return;
-         msg += len;
-         msglen -= len;
-    }
-}
-
 int image_file(int newsock_fd, char *file_path, int type){ //REWRITE THIS CODE 
+    
     FILE *fp;
     char *buf, header[1024], *file_type;
     int fsize, hsize, nbytes;
@@ -392,14 +233,16 @@ int image_file(int newsock_fd, char *file_path, int type){ //REWRITE THIS CODE
         return -1;
     }
 
+    // Get size of image size
     fseek(fp, 0, SEEK_END);
     fsize = ftell(fp);
     rewind(fp);
 
     //Get correct file type for the header
-    if(type == IMAGE_JPEG){
+    if (type == IMAGE_JPEG) {
         file_type = "jpeg";
-    }else{
+    }
+    else {
         file_type = "gif";
     }
 
@@ -409,8 +252,8 @@ int image_file(int newsock_fd, char *file_path, int type){ //REWRITE THIS CODE
 
     write(newsock_fd, header, hsize);
 
-    buf = (char*)malloc(4096);
-    while((nbytes = fread(buf, sizeof(char), 4096, fp)) > 0){
+    buf = (char*) malloc(4096);
+    while ((nbytes = fread(buf, sizeof(char), 4096, fp)) > 0){
         write(newsock_fd, buf, nbytes);
     }
     free(buf);
@@ -418,6 +261,7 @@ int image_file(int newsock_fd, char *file_path, int type){ //REWRITE THIS CODE
     return 0;
 }
 
+// STILL NEED TO ADD HEADERS FOR HTML)
 int html_file(int newsock_fd, char *request) {
     char *file_buf;
     FILE *f;
@@ -442,11 +286,9 @@ int html_file(int newsock_fd, char *request) {
 
     // Write buffer to socket
     n = write(newsock_fd, file_buf, fsize);
+    free(file_buf);
     return 0;
 }
-
-
-
 
 int connection_handler(int newsock_fd) {
     int n;
@@ -466,7 +308,7 @@ int connection_handler(int newsock_fd) {
     // Tokenize request packet, get request token, and add a period
     token = strtok(buf, " ");
     token = strtok(NULL, " ");
-    pathname = calloc(255, 1);
+    pathname = calloc(strlen(token)+1, 1);
     strcat(pathname, ".");
     strcat(pathname, token);
     
@@ -491,11 +333,96 @@ int connection_handler(int newsock_fd) {
             cgi_script(newsock_fd, pathname);
             break;
         default:
-            printf("--------------------------------\n");
             return_404(newsock_fd);
             break;
     }
     close(newsock_fd);
     
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        fprintf(stderr,"Error, no port provided.");
+        exit(1);
+    }
+
+    int webcache, multithreading;
+    webcache = 0;
+    multithreading = 0;
+
+    if (argc > 2){
+        if (strcmp(argv[2], "multi") == 0) {
+            multithreading = 1;
+        } else if(strcmp(argv[2], "cache") == 0){
+            webcache = 1;
+        }
+        if(argc > 3){
+            if(strcmp(argv[3], "cache") == 0){
+                webcache = 1;
+            }
+            else if(strcmp(argv[3], "multi") == 0){
+                multithreading = 1
+            }
+        }
+    }
+    if (webcache == 1) {
+        printf("Web cache initiated!!\n");
+        printf("Enter cache size: ");
+        gets(cachesize);
+    }
+    if (multithreading == 1) {
+        printf("multithreading initiated!\n");
+    }
+    
+    int port;
+    int sock_fd;
+    int newsock_fd;
+    socklen_t client_len;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+    
+    // Get port argument, convert to integer
+    port = atoi(argv[1]);
+    
+    // Allocate memory for the server socket address structure
+    memset((char *) &server_addr, 0, sizeof(server_addr));
+    
+    // Set attributes for server socket
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+    
+    // Try to open an internet socket, save is file descriptor
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Error opening socket, exiting...");
+        exit(1);
+    }
+    
+    // Bind server IP and port to the socket
+    if (bind(sock_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        perror("Error binding socket, exiting...");
+        exit(1);
+    }
+    
+    // Allow the socket to accept connections
+    listen(sock_fd, 5);
+    
+    puts("Waiting for incoming connections...");
+    client_len = sizeof(client_addr);
+    
+    // Extract the first connection in queue, create a new socket for
+    // it with the same type and address as the original, return a new
+    // socket file descriptor in newsock_fd
+    while ((newsock_fd = accept(sock_fd, (struct sockaddr *)&client_addr, (socklen_t*)&client_len))) {
+        puts("Received a request");
+        connection_handler(newsock_fd);
+    }
+    
+    if (newsock_fd < 0) {
+        perror("Error, connection was not accepted");
+        return -1;
+    }
     return 0;
 }
